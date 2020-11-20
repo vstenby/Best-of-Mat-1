cd37e0312e4b8eaaba4376f52ea0316e8dc6c768
Viktor Stenby <s174483@student.dtu.dk>
2020-11-20 17:38:33 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,13 @@
-#Settings for Best of Mat 1 clipperimport numpy as npsettings = {'min_rating' : 1,            'max_duration' : np.inf,            'semesters' : 'all',            'outputtype' : '.mp3'}
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Settings for Best of Mat 1 clipper
+import numpy as np
+
+settings = {'min_rating' : 1,
+            'max_duration' : np.inf,
+            'semesters' : 'all',
+            'outputtype' : '.mp3'}
+
+
+
