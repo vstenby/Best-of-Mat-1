6f12ff6b007eefb0ba65c793b68e5976520ec1b7
vstenby <s174483@student.dtu.dk>
2021-04-03 13:55:52 +0200

    Major overhaul, ver 2.0.0

--- bom1.py
+++ bom1.py
@@ -1,143 +1,218 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 16 17:19:05 2020
 
-@author: viktorstenby
-"""
-
-import moviepy
-from moviepy.editor import VideoFileClip
 import pandas as pd
 import os
-import unicodedata
 import numpy as np
-import re
 
-def clip(pathin, t1 : int, t2 : int, pathout):
-    if t2 < t1: 
-        return
+def welcome():
+    st=["""
+      ____            _            __   __  __       _     __ 
+     |  _ \          | |          / _| |  \/  |     | |   /_ |
+     | |_) | ___  ___| |_    ___ | |_  | \  / | __ _| |_   | |
+     |  _ < / _ \/ __| __|  / _ \|  _| | |\/| |/ _` | __|  | |
+     | |_) |  __/\__ \ |_  | (_) | |   | |  | | (_| | |_   | |
+     |____/ \___||___/\__|  \___/|_|   |_|  |_|\__,_|\__|  |_|
+        """,
+        '     '+'Best of Mat 1: Release 2.0.0 (03/04/2021)'.center(57),
+        '',
+        'Author: Viktor Stenby Johansson',
+        'If you have any problems with this software, feel free to reach out to me via Facebook.',
+        '']
+    for s in st: print(s)
+    return
+
+def load_clips():
+    #Read in the metadata, download links etc. etc.
+    metadata = pd.read_csv('./csv/metadata.csv',sep='\t')
+
+    csvs = [x for x in os.listdir('./csv') if (x.endswith('.csv')) and (x != 'metadata.csv')]
     
-    clip = VideoFileClip(filename=pathin).subclip(t1,t2)
+    #Sort by all sorts of stuff. Basically make sure that the csvs are loaded in the right order.
+    csvs.sort(key = lambda element: (int(element[1:3]), reversor(element[0]), element[3], element[6:8]))
+
+    clips = pd.DataFrame(columns=['tag', 'nclip', 'name', 't1', 't2', 'rating'])
+
+    for csv in csvs:
+        temp = pd.read_csv('./csv/'+csv, sep='\t')
+        temp['tag'] = csv.split(' ')[0]
+        temp['nclip'] = np.arange(1, len(temp)+1)
+        clips = clips.append(temp)
+
+    clips = clips.merge(metadata, on='tag')
+    clips['duration'] = [ts_to_int(duration(x,y)) for x, y in zip(clips['t1'], clips['t2'])]
+    clips['name'] = clips['name'].apply(lambda x : x.strip())
     
-    if pathout.endswith('.mp4'):
-        clip.write_videofile(pathout, temp_audiofile="./export/temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac")
-        clip.close()
-    elif pathout.endswith('.mp3'):
-        audio = clip.audio
-        audio.write_audiofile(pathout)
-        audio.close()
+    return clips
+                                                                   
+
+                                             
+def fetch_info(urls):
+    from getpass import getpass
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    import os
+    import json
     
+    '''
+    Input: 
+        urls, either a single or list of urls pointing to a lecture,
+        e.g. https://video.dtu.dk/media/Uge+11A+Symmetriske+matricer+-+del+1/0_knty601o/185135
+        
+    Output:
+        (stream_titles, stream_links, download_links)
+        stream_titles  are the lecture title, where ':', ',' and '.' is removed.
+        stream_links   are the links to the streamed version of the lecture.
+        download_links are the links to downloading the lecture. 
+    '''
     
-def fetch_semesters():
-    semesters = os.listdir('./tsv')
-    semesters = [x for x in semesters if x.startswith('E') or x.startswith('F')]
-    return semesters
+    username = input("Please enter your DTU login: ")
+    password = getpass("Please enter your DTU password: ")
     
-def convert_timestamp(s):
-    #MM:SS or HH:MM:SS
-    ssplit = s.split(':'); ssplit = [int(x) for x in ssplit]
-    if len(ssplit) == 2:
-        if ssplit[0] > 59 or ssplit[1] > 59: 
-            raise ValueError('Invalid timestamp.')
-        return ssplit[0]*60 + ssplit[1]
-    elif len(ssplit) == 3:
-        #HH:MM:SS
-        if ssplit[1] > 59 or ssplit[2] > 59: 
-            raise ValueError('Invalid timestamp.')
-        return ssplit[0]*60*60 + ssplit[1]*60 + ssplit[2]
+    if not os.path.isfile('./chromedriver'):
+            raise Exception('Chromedriver cannot be located.')
+            
+    # Specify window to not open
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+
+    driver = webdriver.Chrome('./chromedriver',options=options)
+
+    driver.get('https://video.dtu.dk/user/login')
+
+    elem = driver.find_element_by_name('Login[username]')
+    elem.clear()
+    elem.send_keys(username)
+
+    elem = driver.find_element_by_name('Login[password]')
+    elem.clear()
+    elem.send_keys(password)
+
+    print('Logging into video.dtu.dk!')
+    elem.send_keys(Keys.RETURN)
+
+    try:
+        elem = driver.find_element_by_class_name('formError')
+        print('Wrong username or password.')
+    except:
+        print('Succesfully logged into video.dtu.dk!')
+        
+    if type(urls) is not list: urls = [urls]
+    
+    stream_titles   = []
+    stream_links    = []
+    download_links  = []
+    
+    for i, url in enumerate(urls):
+        
+        #Move driver to that url.
+        driver.get(url)
+        
+        stream_titles.append(driver.find_element_by_class_name('entryTitle').text\
+                                   .replace(':','').replace(',','')\
+                                   .replace('Ã¥', 'aa').replace('Ã¦', 'ae')) 
+                                    #We might need to replace more letters eventually.
+        
+        driver.switch_to.frame(driver.find_element_by_css_selector('#kplayer_ifp'))
+        script = driver.find_element_by_css_selector('body script:nth-child(2)').get_attribute("innerHTML")
+        data = (script.splitlines()[2])[37:-1]
+        
+        # Load the data into json format
+        js = json.loads(data)
+        
+        #Get the stream link and download link.
+        stream_links.append(js["entryResult"]["meta"]["dataUrl"])
+        download_links.append(js["entryResult"]["meta"]["downloadUrl"])
+        
+    #If we only queried for one url.
+    if len(stream_titles) == 1: 
+        stream_titles  = stream_titles[0]
+        stream_links   = stream_links[0]
+        download_links = download_links[0]
+    
+    return stream_titles, stream_links, download_links
+
+#Class used to sort the csvs.
+class reversor:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def __eq__(self, other):
+        return other.obj == self.obj
+
+    def __lt__(self, other):
+        return other.obj < self.obj
+
+def ts_to_int(ts):
+    '''
+    Helper function to convert timestamp to int.
+    '''
+    if type(ts) is str:
+        if ':' in ts:
+            #We either have HH:MM:SS or HH:SS
+            if ts.count(':') == 1:
+                #MM:SS
+                hh     = 0
+                mm, ss = ts.split(':')
+                mm, ss = int(mm), int(ss)
+
+            elif ts.count(':') == 2:
+                #HH:MM:SS
+                hh, mm, ss = ts.split(':')
+                hh, mm, ss = int(hh), int(mm), int(ss)
+        #Check that mm < 60 and ss < 60.
+        if not ((mm < 60) and (ss < 60)):
+            raise ValueError(f'Minute or second count is more than 60. Given timestamp: {ts}.')
+        s = ss + mm*60 + hh*60*60
     else:
-        raise ValueError('Wrong number of : in string')
+        s = int(ts)
+    return s
+
+def duration(t1, t2):
     
+    diff = ts_to_int(t2) - ts_to_int(t1)
     
-def read_clips(semesters):
+    #Clip is between 0 seconds and 1 hour.
+    assert (diff > 0) and (diff < 3600)
+
+    minutes, seconds = divmod(diff, 60)
+
+    return f'{minutes:02}:{seconds:02}'
+
+def print_clips(clips):
+    if len(clips) == 0:
+        return
+    clips = clips.reset_index(drop=True)
+    print('Tag'.center(10) + 'Name'.ljust(100) + 'Rating')
+    print('-'*(10+100+6))
+    prev_tag = clips['tag'][0]
+    for tag, name, rating in zip(clips['tag'], clips['name'], clips['rating']):
+        if tag != prev_tag:
+            print('')
+        prev_tag = tag
+        print(tag.ljust(10) + name.ljust(100) + str(rating).rjust(6))
+    return
+
+def ffmpeg_clip(t1, t2, url, pathout):
+    import subprocess
     
-    df = pd.DataFrame(columns=['t1','t2','name','rating','semester','clipnumber'])
+    #Replace letters causing trouble.
+    pathout = pathout.replace(' ','_')\
+                     .replace(',','')\
+                     .replace("'","") 
     
-    for semester in semesters:
-        csvs = os.listdir('./tsv/' + semester); 
-        csvs = [x for x in csvs if x.endswith('.tsv') and x != 'links.tsv']
-       
-        for csv in csvs:
-            dftemp = pd.read_csv('./tsv/'+semester+'/'+csv,sep='\t',encoding='utf-8')
-            dftemp['lecture']  = csv.replace('_',' ').replace('.tsv','')
-            dftemp['semester'] = semester
-            dftemp['clipnumber'] = np.arange(len(dftemp))+1
-            df = df.append(dftemp)
+    if pathout.endswith('.mp3'):
+        bashcmd = f'ffmpeg -ss 00:{t1}.00 -i "{url}" -t 00:{duration(t1,t2)}.00 -q:a 0 -map a {pathout} -loglevel error'
+    elif pathout.endswith('.mp4'):
+        #bashcmd = f'ffmpeg -ss 00:{t1}.00 -i "{url}" -t 00:{duration(t1,t2)}.00 -c copy {pathout} -loglevel error'
+        bashcmd = f'ffmpeg -ss 00:{t1}.00 -i "{url}" -t 00:{duration(t1,t2)}.00 {pathout} -loglevel error'
+    else:
+        raise ValueError('Wrong output format.')
             
-    df = df.reset_index(drop=True)
+    rtrn = subprocess.call(bashcmd, shell=True)
+    
+    return rtrn
+    
 
-    return df
-
-def read_all_clips():
-    semesters = fetch_semesters()
-    df = read_clips(semesters)
-    return df
-
-def read_links(semesters):
-    df = pd.DataFrame(columns=['n', 'lecture', 'link', 'semester'])
-    for semester in semesters:
-        try:
-            dftemp = pd.read_csv('./tsv/' + semester + '/links.tsv',sep='\t',encoding='utf-8')
-            dftemp['semester'] = semester
-        except:
-            dftemp = pd.DataFrame(columns=['n', 'lecture', 'link', 'semester'])
-            
-        df = df.append(dftemp)
-        
-   
-    return df
-
-def read_all_links():
-    semesters = fetch_semesters()
-    df = read_links(semesters)
-    return df
-
-def start_driver():
-    import sys
-    sys.path.insert(1, 'DTU-Video-Downloader')
-    import DTU_VD_functions  as VDfunc   
-    
-    config = VDfunc.prompt_config()
-    driver = VDfunc.open_driver(config, 'https://video.dtu.dk/user/login')
-    
-    return driver
-
-def download_links(df):
-    links = list(df['link'])
-    names = './downloads/' + df['semester'] + ' ' + df['n'].apply(lambda x : str(x).zfill(2)).astype(str) + ' ' + df['name']
-    names = [x.replace(' ','_') for x in names]
-    
-    import sys
-    sys.path.insert(1, '/DTU-Video-Downloader/DTU_VD_functions.py')
-    
-    config = prompt_config()
-    driver = open_driver(config, 'https://video.dtu.dk/user/login')
-    
-    download_videos(driver,links,pathout=names)
-    
-def generate_df():
-    df_clip = read_all_clips(); #df_clip['lecture'] = df_clip['lecture'].apply(lambda x : unicodedata.normalize('NFD', x))
-    df_link = read_all_links(); #df_link['lecture'] = df_link['lecture'].apply(lambda x : unicodedata.normalize('NFD', x))
-    
-    df = pd.merge(df_clip, df_link, how='left', on=['lecture','semester'])
-    df = df.sort_values(['semester','n'])
-    
-    df['path'] = './downloads/' + df['semester'] + '_' + df['n'].astype(str).str.zfill(2) + '_' + df['lecture'].str.replace(' ','_') + '.mp4'
-    df['clippath'] = './export/' + df['semester'] + ' L' + df['n'].astype(str).str.zfill(2) + ' C' + df['clipnumber'].astype(str).str.zfill(2)+' R' + df['rating'].astype(str).str.zfill(2)+' '+df['name']
-    
-    #Convert timestamps
-    df['t1'] = df['t1'].apply(lambda x : convert_timestamp(x))
-    df['t2'] = df['t2'].apply(lambda x : convert_timestamp(x))
-    
-    if os.name == 'nt':
-        df['name'] = df['name'].apply(lambda x : re.sub(r'[\\/*?:"<>|]',"",x))
-
-    return df
-    
-    
-        
-    
-        
     
     
--- main.py
+++ main.py
@@ -1,110 +1,176 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# -- Dependencies --
-import sys
+# Dependencies
 import os
 import numpy as np
+import pandas as pd
+from bom1 import *
+import argparse
+import difflib
+import re
+import shutil
+
 import time
 
-from bom1 import *
-from settings import *
-
-#These packages usually cause the most trouble.
-import moviepy
-import selenium
-import youtube_dl
-
-#Import DTU-Video-Downloader
-sys.path.insert(1, 'DTU-Video-Downloader')
-try:
-    import DTU_VD_functions  as VDfunc
-except ImportError:
-    raise ImportError('Unable to import DTU-Video-Downloader. '\
-                      +'Download it here: https://github.com/vstenby/DTU-Video-Downloader '\
-                      +'and check folder structure.')
-
-#Check if chromedriver exists
-if not os.path.exists('chromedriver'):
-    raise ImportError('chromedriver not found. Download the corresponding version here: https://chromedriver.chromium.org/downloads')
-
 def main():
+    start_time = time.time()
+    
+    #Print the welcome message.
+    welcome()
+    
+    #Load the clips
+    clips = load_clips()
+    
+    #Set up the arguments. 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--list', default=False, action='store_true', help='print the list of clips instead of actually exporting them.')
+    
+    parser.add_argument('--clipname', default=None, type=str,
+                        help='specify which clip name you want to export.')
+    parser.add_argument('--minrating', default=1, type=int,  choices = range(1,11),
+                        help='only export clips with rating >= minrating.')
+    parser.add_argument('--maxrating', default=10, type=int, choices = range(1,11),
+                        help= 'only export clips with rating <= maxrating.') #Why anyone ever would use this, I don't know.
+    parser.add_argument('--minduration', default=0, type=int,
+                        help= 'only export clips with duration >= minduration. duration is in seconds.')
+    parser.add_argument('--maxduration', default=np.inf, type=int,
+                        help='only export clips with duration <= maxduration. duration is in seconds.')
+    parser.add_argument('--tag', default='', type=str,
+                        help='regex for specfiying which tag you want to export.')
+    
+    parser.add_argument('--mint1', default=0, type=int, 
+                        help='only export clips with mint1 <= t1.')
+    parser.add_argument('--maxt1', default=np.inf, type=int,
+                        help='only export clips with t1 <= maxt1.')
+    parser.add_argument('--mint2', default=0, type=int,
+                        help='only export clips with mint2 <= t2.')
+    parser.add_argument('--maxt2', default=np.inf, type=int,
+                        help='only export clips with t2 <= maxt2.')
+    
+    parser.add_argument('--filetype', default='mp3', type=str, choices=['mp3', 'mp4'], help='filetype to export as - either mp3 or mp4.')
+    parser.add_argument('--noprefix', default=False,  action='store_true', help='include prefix specifying info about the clip.')
+    parser.add_argument('--clearexport', default=False, action='store_true', help='clear the export folder before exporting.')
+    parser.add_argument('--silent', default=False, action='store_true', help='if --silent is passed, then progress is not printed to the console.')
+    #TODO: Add some 
+    
+    args = parser.parse_args()
+        
+    n = len(clips)
+    
+    #Make sure that we have the ./export folder.
+    if not os.path.exists('./export'):
+        os.mkdir('./export')
+            
+    #Construct the masks for each query.
+    if args.clipname is not None:
+        clipname_mask = (clips['name'] == args.clipname).to_numpy()
+    else:
+        clipname_mask = np.ones(n).astype(bool)
+        
+    if args.minrating != 1:
+        minrating_mask = (args.minrating <= clips['rating']).to_numpy()
+    else:
+        minrating_mask = np.ones(n).astype(bool)
 
-    #Check if ./download and ./export are folders
-    needed_folders = ['./downloads', './export']
-    for folder in needed_folders:
-        if not os.path.exists(folder): os.mkdir(folder)
+    if args.maxrating != 10:
+        maxrating_mask = (clips['rating'] <= args.maxrating).to_numpy()
+    else:
+        maxrating_mask = np.ones(n).astype(bool)
+        
+    if args.minduration != 0:
+        minduration_mask = (args.minduration <= clips['duration']).to_numpy()
+    else:
+        minduration_mask = np.ones(n).astype(bool)
+        
+    if args.maxduration != np.inf:
+        maxduration_mask = (clips['duration'] <= args.maxduration).to_numpy()
+    else:
+        maxduration_mask = np.ones(n).astype(bool)
+    
+    if args.tag != '':
+        tags = [tag for tag in clips['tag'].unique() if re.search(args.tag, tag) is not None]
+        tags_mask = (clips['tag'].isin(tags)).to_numpy()
+    else:
+        tags_mask = np.ones(n).astype(bool)
+        
+    if args.mint1 != 0:
+        mint1_mask = args.mint1 <= clips['t1'].apply(lambda x : ts_to_int(x)).to_numpy()
+    else:
+        mint1_mask = np.ones(n).astype(bool)
+        
+    if args.maxt1 != np.inf:
+        maxt1_mask = clips['t1'].apply(lambda x : ts_to_int(x)).to_numpy() <= args.maxt1
+    else:
+        maxt1_mask = np.ones(n).astype(bool)
+        
+    if args.mint2 != 0:
+        mint2_mask = args.mint2 <= clips['t2'].apply(lambda x : ts_to_int(x)).to_numpy()
+    else:
+        mint2_mask = np.ones(n).astype(bool)
+        
+    if args.maxt2 != np.inf:
+        maxt2_mask = clips['t2'].apply(lambda x : ts_to_int(x)).to_numpy() <= args.maxt2
+    else:
+        maxt2_mask = np.ones(n).astype(bool)
+        
+        
+    if args.noprefix:
+        prefix = ''
+    else:
+        prefix = clips['tag']+'_C'+clips['nclip'].astype(str).str.zfill(2)+'_R'+clips['rating'].astype(str).str.zfill(2)+'_'
+    
+    
+    if args.clearexport:
+        if os.path.exists('./export'):
+            shutil.rmtree('./export')
+        os.mkdir('./export')
+        
 
-    #Generate df with all information
-    df = generate_df()
-
-    if not settings['clips'] == 'all':
-        #Extract the clips that we need.
-        clips = np.array(settings['clips'])
-        df = df.loc[np.isin(df['name'].to_numpy(),clips)]
-
-        #Clips specified in settings:
-        clips_found = clips[np.isin(clips, df['name'].to_numpy())]
-        print('Clipping the following clip(s):')
-        for clipf in clips_found: print(clipf)
-        print('')
-
-
-        #Check that all of the clips were found.
-        clips_not_found = clips[np.invert(np.isin(clips,df['name'].to_numpy()))]
-        if len(clips_not_found) != 0:
-            print('Error finding the following clip(s):')
-            for clipnf in clips_not_found:
-                print(clipnf)
-            print('')
-
-    #Extract valid clips.
-    df = df.loc[df['rating'].to_numpy() >= settings['min_rating']]
-    df = df.loc[(df['t2'].to_numpy() - df['t1'].to_numpy()) <= settings['max_duration']]
-
-    if settings['semesters'] == 'all':
-        settings['semesters'] = [x for x in os.listdir('./tsv') if not x.startswith('.')]
-
-    df = df.loc[np.isin(df.semester.to_numpy(), settings['semesters'])]
-
-    driver = None
-
-    if len(df) == 0:
-        print('No clips to be clipped')
+    #Stitch together the outpath.
+    clips['outpath'] = ('./export/'+prefix+clips['name']+'.'+args.filetype).str.replace(' ','_')\
+                                                                               .replace(',','')\
+                                                                               .replace("'","") 
+    
+    #Combine all of the masks into a final single mask, and cut out the relevant clips.
+    final_mask = (clipname_mask) & (minrating_mask) & (maxrating_mask) & (minduration_mask) & (maxduration_mask) & (tags_mask)\
+                 & (mint1_mask) & (maxt1_mask) & (mint2_mask) & (maxt2_mask)
+    
+    clips_final = clips.copy().loc[final_mask]
+    
+    #Check if there are any clips.
+    if len(clips_final) == 0:
+        if not np.any(clipname_mask):
+            print('There are no clips with the specified name.')
+            close_match = difflib.get_close_matches(args.clipname, list(clips['name']))
+            if close_match != []:
+                print(f'Did you perhaps mean {close_match}?')
+        else:
+            print('No clips met the specified query.')
+        return
+    
+    if args.list:
+        print_clips(clips_final)
         return
     else:
-        print(f'A total of {len(df)} clips were found to be exported.')
-
-    for i in range(len(df)):
-        clippath = df['clippath'].iloc[i] + settings['outputtype']
-        if os.path.exists(clippath):
-            #This clip already exists and therefore, it should be skipped.
-            continue
-
-        lecturepath = df['path'].iloc[i]
-
-        # -- Download check  --
-        if not os.path.exists(lecturepath):
-            #Clear the downloaded folder
-            for file in os.listdir('./downloads/'):
-                os.remove('./downloads/' + file)
-
-            link = df['link'].iloc[i];
-            if driver is None:
-                print('It looks like we need to download a video!')
-                config = VDfunc.prompt_config()
-                driver = VDfunc.open_driver(config, 'https://video.dtu.dk/user/login')
-
-            #Download video
-            VDfunc.download_videos(driver, [link], pathout = [lecturepath.replace('.mp4', '')])
-        # -- End of download check --
-
-        t1 = df['t1'].iloc[i]
-        t2 = df['t2'].iloc[i]
-
-        #Do the actual clipping
-        clip(lecturepath, t1, t2, clippath)
-
-
+        n = len(clips_final)
+        if n > 1:
+            input(f'A total of {n} clips were found. Press enter to export. ') #Ask for confirmation if several clips are exported.
+            print('')
+            
+        for t1, t2, url, outpath, i in zip(clips_final['t1'], clips_final['t2'], clips_final['stream_link'], clips_final['outpath'], range(0, n)):
+            rtrn = ffmpeg_clip(t1,t2,url,outpath)
+            progress = f'({i+1}/{n})'.ljust(9)
+            if not args.silent:
+                if not rtrn:
+                    print(f'{progress} {outpath} succesfully exported.')
+                else:
+                    print(f'{progress} {outpath} was not exported.')
+                    
+        end_time = time.time()
+        print('')
+        print(f'Time elapsed: {end_time-start_time} seconds.')
+        return
+                
 if __name__ == '__main__':
     main()
